e865ee936dc893aea7c10bc997fe6a6e53b22eb5
Adam Spannbauer <SpannbauerAdam@gmail.com>
2019-03-23 14:38:20 -0400

    add additional internal data

---
+++ internal_data/write_internal_data.R
@@ -0,0 +1,10 @@
+source('internal_data/regexes.R')
+source('internal_data/comparison_symbols.R')
+
+usethis::use_data(
+  COMPS,                      # Character vector of valid equality comparison symbols
+  CANONICAL_PACKAGE_NAME_RE,  # String contain regex to match valid package names by CRAN standards
+  PACKAGE_RES,                # Character vector of regexes to match package names used in code (i.e. `library(name)`)
+  internal = TRUE,
+  overwrite = TRUE
+)
