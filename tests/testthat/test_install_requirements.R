ad8acf01375e985994ebd5112cfb12e3e9d6dc41
Mark Ewing <mewing@eastman.com>
2019-03-30 17:25:32 -0400

    added reading and processing tests

--- tests/testthat/test_install_requirements.R
+++ tests/testthat/test_install_requirements.R
@@ -1,8 +1,15 @@
 context('general_helpers')
 
 test_that('Reading requirements files', {
+  expect_type(read_requirements_file('testdata/requirements_1.txt'),'character')
   expect_equal(read_requirements_file('testdata/requirements_1.txt'), c("mgsub >= 1.5.0", "lexRankr == 0.4.*", "readOffice < 1", "whitechapelR >= 0.3"))
   expect_equal(read_requirements_file('testdata/requirements_2.txt'), c("mgsub >= 1.5.0", "lexRankr == 0.4.*", "readOffice ~= 0", "whitechapelR >= 0.3", "dplyr != 0.7.*"))
+})
+
+test_that('Processing requirements files',{
+  expect_type(process_requirements_file('testdata/requirements_1.txt'),'list')
+  expect_length(process_requirements_file('testdata/requirements_1.txt'), 7)
+  expect_length(process_requirements_file('testdata/requirements_1.txt')[['url']], 0)
 })
 
 
@@ -27,7 +34,6 @@ test_that('Requirements Generate Correctly', {
 })
 
 test_that('Getting available package versions', {
-  browser()
   expect_equal(get_available_versions('mgsub')[1:4],c("1.0.0", "1.5.0", "1.7.0", "1.7.1"))
   expect_equal(get_available_versions('lexRankr')[1:7],c("0.1.0", "0.2.0", "0.3.0", "0.4.0", "0.4.1", "0.5.0", "0.5.2"))
   expect_equal(get_available_versions('dplyr')[1:24],c("0.1.1", "0.1.2", "0.1.3", "0.1", "0.2", "0.3.0.1", "0.3.0.2", 
