72d1824d39498d3280e56fd98d7252b21fb4b9b3
Spannbauer, Adam M <u775749@emn.com>
2019-04-01 11:39:21 -0400

    fix formatting lint issues in requirements_helpers.R

--- R/requirements_helpers.R
+++ R/requirements_helpers.R
@@ -1,15 +1,19 @@
-activate_packrat = function(){
+activate_packrat = function() {
   tryCatch(packrat::status(),
-           error=function(x){packrat::init();packrat::on()})
+           error = function(x) {
+             packrat::init()
+             packrat::on()
+           })
+
   return(NULL)
 }
 
 
-legal_r_package_name = function(name){
+legal_r_package_name = function(name) {
   #' @param name package name to be checked for validity
   #' @return logical vector of names being legal r package names
 
-  return(grepl(CANONICAL_PACKAGE_NAME_RE_STANDALONE,name))
+  return(grepl(CANONICAL_PACKAGE_NAME_RE_STANDALONE, name))
 }
 
 
@@ -23,11 +27,11 @@ equivalent_version = function(existing, target) {
   #' @return TRUE if versions are equivalent; otherwise FALSE
   #'
   #' @examples
-  #' equivalent_version('1.0.1', '1.*')
+  #' equivalent_version("1.0.1", "1.*")
   #' # [1] TRUE
-  #' equivalent_version('1.9.1', '1.*')
+  #' equivalent_version("1.9.1", "1.*")
   #' # [1] TRUE
-  #' equivalent_version('2.0.0', '1.*')
+  #' equivalent_version("2.0.0", "1.*")
   #' # [1] FALSE
   #'
   # Convert wildcarded versions to regex
@@ -47,19 +51,19 @@ compare_compatible_version = function(existing, target) {
   #' @return TRUE if versions are compatible; otherwise FALSE
   #'
   #' @examples
-  #' compare_compatible_version('1.1.1', '1.*')
+  #' compare_compatible_version("1.1.1", "1.*")
   #' # [1] TRUE
-  #' compare_compatible_version('1.1.1', '1.0')
+  #' compare_compatible_version("1.1.1", "1.0")
   #' # [1] TRUE
-  #' compare_compatible_version('1.1.1', '2.0')
+  #' compare_compatible_version("1.1.1", "2.0")
   #' # [1] FALSE
 
   # Replace trailing version specifier with wildcard (i.e. 2.2 -> 2.*)
-  compatible_version_wildcard = sub('(.*\\.)(.+)', '\\1*', target)
+  compatible_version_wildcard = sub("(.*\\.)(.+)", "\\1*", target)
 
   # Per PEP 440 `~= 2.2` is equivalent to `>= 2.2 & == 2.*`
-  compare_version(existing, compatible_version_wildcard, '==') &
-    compare_version(existing, target, '>=')
+  compare_version(existing, compatible_version_wildcard, "==") &
+    compare_version(existing, target, ">=")
 }
 
 
@@ -74,18 +78,18 @@ compare_version = function(existing, target, comp) {
   #' @return logical(1) indicating if comparison is TRUE
   #'
   #' @examples
-  #' compare_version('1.0.1', '1.*', '==')
+  #' compare_version("1.0.1", "1.*", "==")
   #' [1] TRUE
-  #' compare_version('1.0.1', '1.*', '~=')
+  #' compare_version("1.0.1", "1.*", "~=")
   #' [1] TRUE
-  #' compare_version('1.0.1', '1.*', '!=')
+  #' compare_version("1.0.1", "1.*", "!=")
   #' [1] FALSE
 
   # Push compatible version check to dedicated helper
-  if (comp == "~=") return(compare_compatible_version(existing, target))
+  if (comp == "~=") return(compare_compatible_version(existing, target))  # nolint
 
   # Check if existing == target would satisfy comparison
-  is_equality_check = comp %in% c('>=', '<=', '~=', '==')
+  is_equality_check = comp %in% c(">=", "<=", "~=", "==")  # nolint
 
   # Check if `target` is equivalent to `existing` wrt wildcards
   is_equivalent_version = equivalent_version(existing, target)
@@ -94,7 +98,7 @@ compare_version = function(existing, target, comp) {
   if (comp == "!=" & is_equivalent_version) return(FALSE)
 
   # TODO: definitively prove this is a valid substitution
-  non_wild_card_target = gsub('*', '0', target, fixed = TRUE)
+  non_wild_card_target = gsub("*", "0", target, fixed = TRUE)
 
   existing_version = package_version(existing)
   target_version = package_version(non_wild_card_target)
@@ -103,13 +107,13 @@ compare_version = function(existing, target, comp) {
 }
 
 
-get_installed = function(dummy=NULL){
+get_installed = function(dummy=NULL) {
   #' @param dummy dummy data to return for testing purposes
   #' @return named vector of package versions
-  if(!is.null(dummy)){
+  if (!is.null(dummy)) {
     return(dummy)
   } else {
-    return(installed.packages()[,3])
+    return(installed.packages()[, 3])
   }
 }
 
@@ -119,12 +123,12 @@ get_available_versions = function(package) {
   #' @return character vector of available package versions; NULL if package not found
   #'
   #' @examples
-  #' get_available_versions('mgsub')
+  #' get_available_versions("mgsub")
   #' # [1] "1.0.0" "1.5.0" "1.7.0" "1.7.1"
   #'
-  #' get_available_versions('mgsub_fakerino')
+  #' get_available_versions("mgsub_fakerino")
   #' # NULL
-  crandb_url = paste0("http://crandb.r-pkg.org/", package, "/all")
+  crandb_url = paste0("http://crandb.r-pkg.org/", package, "/all")  # nolint
   response = httr::GET(crandb_url)
 
   if (httr::status_code(response) == 404) return(NULL)
@@ -136,14 +140,15 @@ get_available_versions = function(package) {
 }
 
 
-vmess = function(x,v){
+vmess = function(x, v) {
   #' @param x what message should be printed?
   #' @param v should the message actually be printed?
   #' @return nothing important
   #' @details To help with verbose message printing
 
-  if(v){
+  if (v) {
     message(x)
   }
+
   invisible(return(NULL))
 }
