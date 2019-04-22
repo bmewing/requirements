8d34a0363695f5996f37994fa77bdc3b6a163a19
Adam Spannbauer <SpannbauerAdam@gmail.com>
2019-04-22 15:10:09 -0400

    Revert "gen requirements from packrat lock file (#71)" (#75)
    This reverts commit fbe69751df94216dada6764f8dcfa846ce16491f.

--- internal_data/write_internal_data.R
+++ internal_data/write_internal_data.R
@@ -1,5 +1,5 @@
-source('internal_data/regexes.R')
 source('internal_data/comparison_symbols.R')
+source('internal_data/regexes.R')
 source('internal_data/messages.R')
 
 usethis::use_data(
@@ -27,6 +27,9 @@ usethis::use_data(
   REQ_FILE_EXIST_ERR,           # Error message for non-existing requirements file
   REQ_FILE_EMPTY_ERR,           # Error message for empty requirements file
   REQ_FILE_RESOLUTION_ERR,      # Error message for non-resolvable requirements requirements file
+  REQ_FILE_INVALID_COMP,        # Error message for invalid comparison operator
+  VALID_REQ,                    # Pattern for validating versioned requirements
+  COMP_EXTRACTOR,               # Pattern for extracting comparison operator
   CANONICAL_PACKAGE_NAME_RE_STANDALONE,  # String contain regex to match valid package names by CRAN standards
   PACKAGE_RES,                # Character vector of regexes to match package names used in code (i.e. `library(name)`)
   AUTO_GEN_COMMENTS,          # Comments to be placed at the top of auto-generated requirements files
